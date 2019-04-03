33f4baef801a43f301fcab7d4a5967a9c5729357
Jahaziel <jahaziel.vaz@gmail.com>
2019-04-02 23:40:12 -0600

    Figuring out how to clear my events

--- index.js
+++ index.js
@@ -4,14 +4,41 @@ let containers = [
   getElem('cont1'),
   getElem('life-one'),
   getElem('life-two'),
-  getElem('life-main')
+  getElem('life-main'),
+  getElem('cont2'),
+  getElem('is-one'),
+  getElem('is-two'),
+  getElem('is-main')
 ]
 //
-let [containerOne, lifeOne, lifeTwo, lifeMain] = containers;
+let [containerOne, lifeOne, lifeTwo, lifeMain, containerTwo, isOne, isTwo, isMain] = containers;
+
+
 
 let expandFunc = () => {
   lifeOne.innerHTML = "MAKE";
   lifeTwo.innerHTML = "HAPPEN";
   lifeMain.classList.add('main-text');
   containerOne.classList.add('container-two');
+
+
 }
+
+let expandFunc2 = () => {
+  isOne.innerHTML = "CHANGE";
+  isTwo.innerHTML = "FREEDOM";
+  isMain.classList.add('main-text');
+  containerTwo.classList.add('container-two');
+}
+
+containerOne.addEventListener('click', expandFunc);
+containerOne.removeEventListener('click', expandFunc);
+containerTwo.addEventListener('click', expandFunc2);
+
+let eventRemover = () => {
+  if(expandFunc){
+    containerTwo.removeEventListener('', expandFunc2);
+  }
+}
+
+eventRemover();
