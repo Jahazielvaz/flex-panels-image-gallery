4c005cf3f8e59f052213296281bedaff5406ff33
Jahaziel <jahaziel.vaz@gmail.com>
2019-04-02 22:04:42 -0600

    Got the first working section

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-let containers = [`${document.getElementById('cont1')}`, `${document.getElementById('cont1')}`,
-`${document.getElementById('cont1')}`,
-`${document.getElementById('cont1')}`,
-`${document.getElementById('cont1')}`]
-
-let [container1, container2, container3, container4, container5] = containers;
+let expandFunc = () => {
+  let lifeOne = document.getElementById('life-one');
+  let lifeTwo = document.getElementById('life-two');
+  let containerOne = document.getElementById('cont1');
+  let lifeMain = document.getElementById('life-main');
+  lifeOne.innerHTML = "MAKE";
+  lifeTwo.innerHTML = "HAPPEN";
+  lifeMain.classList.add('main-text');
+  containerOne.classList.add('container-two');
+}
